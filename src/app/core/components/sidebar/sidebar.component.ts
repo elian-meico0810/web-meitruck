3b539746c39f0cf7fc1d1e04240e6c54337bd268
elian-meico0810 <efince@meico.com.co>
2026-02-03 11:48:17 -0500

    add sibear base by proyect the meico

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MenuService } from '@core/components/sidebar/services/loadmenu.service';
+import { AppStore, User } from '../../store/app.store';
+import { environment } from 'src/environments/enviroments';
+import { LoginService } from 'src/app/modules/seguridad/pages/login/services/login-service.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,13 +12,23 @@ import { MenuService } from '@core/components/sidebar/services/loadmenu.service'
 export class SidebarComponent implements OnInit {
   public modules: any[] = [];
   public submenuOpen: { [key: string]: boolean } = {};
-
-  constructor(private menuService: MenuService) { }
+  readonly store = inject(AppStore);
+  public userLogin: User = {
+    email: JSON.parse(localStorage.getItem(`AuthUser${environment.idAplicacion}`) || '{}').Email || '',
+    FirstName: JSON.parse(localStorage.getItem(`AuthUser${environment.idAplicacion}`) || '{}').Name || '',
+    Permissions: JSON.parse(localStorage.getItem(`AuthUser${environment.idAplicacion}`) || '{}').JWT || '',
+    userName: JSON.parse(localStorage.getItem(`AuthUser${environment.idAplicacion}`) || '{}').userName || '',
+    Foto: '',
+  };
+  constructor(
+    private loginService: LoginService,
+    private menuService: MenuService
+  ) { }
 
   ngOnInit(): void {
     this.menuService.menu$.subscribe(modules => {
       this.modules = modules;
-      
+
     });
     this.menuService.loadMenu();
   }
@@ -26,5 +39,25 @@ export class SidebarComponent implements OnInit {
 
   hasVisiblePages(module: any): boolean {
     return module.paginas.some((page: any) => page.paginaVisible);
+  }
+  Logout() {
+    this.loginService.logout();
+  }
+
+  OnChangeAccount() {
+    this.loginService.ChangeAccount();
+  }
+
+  sidebarCollapsed = false;
+
+  toggleSidebar() {
+    this.sidebarCollapsed = !this.sidebarCollapsed;
+
+    // Cerrar todos los submenús cuando se contrae la barra
+    if (this.sidebarCollapsed) {
+      for (let module of this.modules) {
+        this.submenuOpen[module.nombreModulo] = false;
+      }
+    }
   }
 }
--- src/environments/enviroments.ts
+++ src/environments/enviroments.ts
@@ -1,12 +1,12 @@
 export const environment = {
   apiAuthUrl: "https://portal.meico.local/auth-meico/",
-  apiAppUrl: "http://localhost:8080/",
+  apiAppUrl:  'http://localhost:4300/',
   idAplicacion: "9",
   azure: {
     clientId: 'a91c9d2f-a0e0-478b-a265-4a1da3d441a3',
     authority: 'https://login.microsoftonline.com/a06d2c92-7803-417f-9812-16af600fee47',
-    loginRedirectUri: 'http://localhost:8080/',
-    logoutRedirectUri: 'http://localhost:8080/',
+    loginRedirectUri: 'http://localhost:4300/',
+    logoutRedirectUri: 'http://localhost:4300/',
     scopes: ['User.Read'],
     uri: 'https://graph.microsoft.com/v1.0/me',
     store_azure:"DefaultEndpointsProtocol=https;AccountName=filestoragemeico;AccountKey=ANbcGV5pXkzntMCIac+XFBC5vdoIN3GanTEdMaHSVBIrFY7ftWoJzZTuoI1ZEigcIuVmWPTzmpdg+ASt7ypWBA==;EndpointSuffix=core.windows.net",
